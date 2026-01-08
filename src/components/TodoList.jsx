0c9a4032d2bd4770418f85ba5bcf5c92d532be8e
Dean Gray <mrdeangray@gmail.com>
2026-01-08 03:25:16 -0500

    3 columns

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,28 +1,35 @@
+
 import React from "react";
 import Todo from "./Todo";
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "../firebase";
 
 const TodoList = ({ todos, onAddSubTodo, onToggleTodo, onToggleSubTodo, onAddTodo, subTodoInputs, setSubTodoInputs, setTodos }) => {
   const handleDeleteTodo = (todoId) => {
     setTodos(todos => todos.filter(t => t.id !== todoId));
   };
   // Custom handler: toggling a todo also toggles all its subtasks
-  const handleToggleTodoWithSubs = (todoId) => {
-    setTodos(todos =>
-      todos.map(todo => {
+  const handleToggleTodoWithSubs = async (todoId) => {
+    setTodos(todos => {
+      const updatedTodos = todos.map(todo => {
         if (todo.id === todoId) {
           const newCompleted = !todo.completed;
-          return {
+          const updatedTodo = {
             ...todo,
             completed: newCompleted,
             subTodos: todo.subTodos.map(sub => ({ ...sub, completed: newCompleted }))
           };
+          // Save to Firestore
+          setDoc(doc(db, "todos", todo.id), updatedTodo);
+          return updatedTodo;
         }
         return todo;
-      })
-    );
+      });
+      return updatedTodos;
+    });
   };
   return (
-    <div className="space-y-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 w-full">
       {todos.map((todo, idx) => (
         <Todo
           key={todo.id}
